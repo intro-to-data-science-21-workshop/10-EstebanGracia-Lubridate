e7414ff9d7c84a3a3f431a6b9bfdff3cd84ca161
zazzooo <lorenzo.gini1998@gmail.com>
2021-10-27 17:53:04 +0200

    the tutorial, finally

--- try_lubridate.R
+++ try_lubridate.R
@@ -31,9 +31,64 @@ dweeks(3.2) # week in seconds, it works with year and day as well
 
 tail(OlsonNames(),23)
 
+duration(90, "minutes")
+
 # most useful function (for me at least)
 d = data.frame(day = c(2,5,23,31), month = c(3,6,12,4), year = c(1996,1998,2003,1345))
+d
 d %>% 
   select(year, month, day) %>% 
   mutate(date = make_datetime(year, month, day))
- 
+
+t <- as.POSIXlt(as.Date('1994-04-12'))
+is.POSt
+
+x <- ymd('1998 oct 23') 
+hour(x) <- 5 # add the hour to our date!
+second(x) <- 34
+dst(x)
+
+
+x <- as.Date("2009-09-02")
+dst(x)
+y <- as.POSIXct("2009-08-03")
+day(x)
+int_diff(c(x,y))
+int <- interval(x,y)
+int_length(int)
+shift <- as.period(interval(as.Date('1998-01-04'), as.Date('1998-02-04'))) # as.period = as.duration
+int_shift(int, shift)
+
+difftime(now() + 5, now()) # time difference
+difftime(as.Date('2021-10-31'), now()) # change unity of misure
+
+x <- as_date("2009-09-02", tz = NULL)
+is.POSIX(x)
+
+is.POSIXt(as.Date("2009-08-03"))
+is.POSIXt(as.POSIXct("2009-08-03"))
+
+dseconds(x = 6) #duration object
+
+x <- c("09-01-01", "09-01-02", "09-01-03", "09-01-2003")
+
+parse_date_time(x, "ymd HM") # won't work
+parse_date_time(x, c("ymd", "dmy"))
+
+parse_date_time(x, "ymd HM", truncated = 3) # it works
+
+parse_date_time(c("2016.2", "2016-04"), orders = "Yq")
+
+date <- ymd("2010-03-03")
+rollbackward(date) # last day of the preview moth
+rollforward(date) # last day of the current moth
+
+
+x <- ymd_hms("2009-08-03 12:01:59.23")
+# floor, celain and round 
+floor_date(x, "day")
+floor_date(x, "season")
+floor_date(x, "halfyear")
+floor_date(x, "year")
+
+
---
+++ tutorial.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(legislatoR)
+library(lubridate)
+library(ggplot2)
+
+
+
+
+
+# cze_core %>%
+#   filter(!is.na(death)) %>% 
+#   mutate(life_duration = interval((birth),(death))) %>%
+#   mutate(as.period(life_duration)) %>%
+#   mutate(as.integer(life_duration / dyears(1)))
+# 
+
+# cze_core %>% 
+#   mutate(month_birth = month(birth, label = T))
+
+ymd('1948-11-13')
+ydm('19982111') 
+ydm(19982111)
+french_revolution <- mdy_hm('jul 14st, 1789 12:34', tz = 'cet')
+french_revolution
+with_tz(french_revolution, tz = 'America/Bogota')
+
+d = data.frame(day = c(2,5,23,31), month = c(3,6,12,4), year = c(1996,1998,2003,1345))
+d
+d %>% 
+  select(year, month, day) %>% 
+  mutate(date = make_datetime(year, month, day))
+
+cze_core <- get_core(legislature = "cze")
+
+cze_core %>%
+  filter(!is.na(death)) %>%
+  mutate(life_duration = interval((birth),(death))) %>%
+  mutate(life_duration %/% years(1))
+
+cze_core %>% 
+  mutate(day_birth = wday(birth, label = T)) %>% 
+  filter(!is.na(day_birth)) %>% 
+  ggplot(aes(x = day_birth)) +
+  geom_bar(fill="black") +
+  labs(x = 'Day of the week' , y = 'Freq', title = "Day of birth")
